7a5f0be96359c4311746265340176236524bd8db
Joakkoo <joako2503@outlook.com>
2025-02-20 20:39:34 -0300

    Paginas y navbar

--- front-facturacion/src/app/page.tsx
+++ front-facturacion/src/app/page.tsx
@@ -1,101 +1,102 @@
-import Image from "next/image";
+import React from 'react'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu"
+import Link from "next/link"
+import { MoveDownLeft, MoveUpRight } from "lucide-react";
 
-export default function Home() {
+
+function page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <div>
+          <NavigationMenu>
+    <NavigationMenuItem>
+      <Link href='/inicio' legacyBehavior passHref>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          Inicio
+        </NavigationMenuLink>
+      </Link>
+      <Link href="/clientes" legacyBehavior passHref>
+        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          Clientes
+        </NavigationMenuLink>
+      </Link>
+      <Link href="/ventas" legacyBehavior passHref>
+      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          Ventas
+        </NavigationMenuLink>
+      </Link>
+      <Link href="/productos" legacyBehavior passHref>
+      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          Productos
+        </NavigationMenuLink>
+      </Link>
+      <Link href="/pagos" legacyBehavior passHref>
+      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          Pagos
+        </NavigationMenuLink>
+      </Link>
+    </NavigationMenuItem>
+  </NavigationMenu>
+   <div className="w-full py-20 lg:py-40">
+   <div className="container mx-auto">
+     <div className="grid text-left grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 w-full gap-4 lg:gap-8">
+       <div className="flex gap-0 flex-col justify-between p-6 border rounded-md">
+         <MoveUpRight className="w-4 h-4 mb-10 text-primary" />
+         <h2 className="text-4xl tracking-tighter max-w-xl text-left font-regular flex flex-row gap-4 items-end">
+           500.000
+           <span className="text-muted-foreground text-sm tracking-normal">
+             +20.1%
+           </span>
+         </h2>
+         <p className="text-base leading-relaxed tracking-tight text-muted-foreground max-w-xl text-left">
+           Monthly active users
+         </p>
+       </div>
+       <div className="flex gap-0 flex-col justify-between p-6 border rounded-md">
+         <MoveDownLeft className="w-4 h-4 mb-10 text-destructive" />
+         <h2 className="text-4xl tracking-tighter max-w-xl text-left font-regular flex flex-row gap-4 items-end">
+           20.105
+           <span className="text-muted-foreground text-sm tracking-normal">
+             -2%
+           </span>
+         </h2>
+         <p className="text-base leading-relaxed tracking-tight text-muted-foreground max-w-xl text-left">
+           Daily active users
+         </p>
+       </div>
+       <div className="flex gap-0 flex-col justify-between p-6 border rounded-md">
+         <MoveUpRight className="w-4 h-4 mb-10 text-success" />
+         <h2 className="text-4xl tracking-tighter max-w-xl text-left font-regular flex flex-row gap-4 items-end">
+           $523.520
+           <span className="text-muted-foreground text-sm tracking-normal">
+             +8%
+           </span>
+         </h2>
+         <p className="text-base leading-relaxed tracking-tight text-muted-foreground max-w-xl text-left">
+           Monthly recurring revenue
+         </p>
+       </div>
+       <div className="flex gap-0 flex-col justify-between p-6 border rounded-md">
+         <MoveUpRight className="w-4 h-4 mb-10 text-primary" />
+         <h2 className="text-4xl tracking-tighter max-w-xl text-left font-regular flex flex-row gap-4 items-end">
+           $1052
+           <span className="text-muted-foreground text-sm tracking-normal">
+             +2%
+           </span>
+         </h2>
+         <p className="text-base leading-relaxed tracking-tight text-muted-foreground max-w-xl text-left">
+           Cost per acquisition
+         </p>
+       </div>
+     </div>
+   </div>
+ </div>
+</div>
+  )
 }
+
+export default page
---
+++ front-facturacion/src/app/ventas/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ventas() {
+  return (
+    <div>ventas</div>
+  )
+}
+
+export default ventas
